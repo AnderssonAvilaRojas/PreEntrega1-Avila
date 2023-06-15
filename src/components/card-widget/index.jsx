129bb4ac0f133f528eda99c2d0517a1b2fc40955
Andersson Avila Rojas <a.avilar2@uniandes.edu.co>
2023-06-14 21:31:52 -0500

    Se agregan navbar y card widget

---
+++ src/components/card-widget/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+const Cardwidget = () => {
+  return (
+<AddShoppingCartIcon/>
+  )
+}
+
+export default Cardwidget;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/nav-bar";
+
+const Home = ()=>{
+
+    return (
+        <div>
+         <Navbar/>  
+         <h1>Titulo</h1>
+         <div>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corporis sit repellendus dicta possimus qui numquam error neque aperiam recusandae quis iusto quibusdam, asperiores alias ratione adipisci ducimus! Velit, delectus!</p> 
+        </div> 
+        </div>
+    );
+}
+export default Home;
